475c81a0c97e30a72456ef421b70c9ba402d78d2
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 09:22:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key] : value})
 }
  
- function deleteFromObjectByKey(object, key) {
-   delete object.key;
- }
+ 
